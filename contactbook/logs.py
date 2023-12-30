3df289c60d4473582ad68a9933f5a2d32268e544
VadimTrubay <vadnetvadnet@ukr.net>
2023-12-30 14:10:07 +0200

    Create logs.py

---
+++ contactbook/logs.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+
+def log(message: str):
+    """
+    The log function takes a string as an argument and writes it to the logs.txt file with a timestamp.
+    :param message: str: Specify the type of data that is expected to be passed into the function
+    :return: Nothing, it just writes the message to a file
+    """
+    current_time = datetime.strftime(datetime.now(), "[%Y-%m-%d] [%H:%M:%S]")
+    full_message = f"{current_time} - {message}"
+    with open("logs.txt", "a") as file:
+        file.write(f"{full_message}\n")
